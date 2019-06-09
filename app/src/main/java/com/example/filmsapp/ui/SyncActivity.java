0c006e5f7f0d369f7282b6c35ed2a786a958e62e
Bruno Felix <brunofelix.dev@gmail.com>
2019-06-09 19:51:27 -0300

    Card View

---
+++ app/src/main/java/com/example/filmsapp/ui/SyncActivity.java
@@ -0,0 +1,15 @@
+package com.example.filmsapp.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.filmsapp.R;
+
+public class SyncActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sync);
+    }
+}
